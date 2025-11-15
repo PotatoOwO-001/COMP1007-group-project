dcd96f2ed2ca2e4eb3fc3fcc22567e8ecee38b4a
PotatoOwO-001 <chinghungben@gmail.com>
2025-11-15 22:18:09 +0800

    Update character paramete.py

--- character paramete.py
+++ character paramete.py
@@ -1,1 +1,18 @@
-import pygame 
+import pygame
+import sys
+import math
+
+pygame.init()
+#character appearence
+class StickMan:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed = 10
+        self.direction = -1  #-1=right, 1=left
+
+        #Body parameter
+        size.head_radiu = 30
+        size.body_length = 50
+        size.hands_length = 40
+        size.legs_length = 60
