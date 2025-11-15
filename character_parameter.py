31bbbe8dbf2c4c9d26896f560c33f2a27b6f1c0c
PotatoOwO-001 <chinghungben@gmail.com>
2025-11-16 03:11:32 +0800

    Update character_parameter.py

--- character_parameter.py
+++ character_parameter.py
@@ -115,12 +115,12 @@ clock = pygame.time.Clock()
 running = True
 
 while running:
-    for event in pygame.event.get():
+    for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False
-        else if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
     
     # Key pressing
     keys = pygame.key.get_pressed()
