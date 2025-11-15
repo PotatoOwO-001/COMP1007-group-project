ab4d72f426c9a1c03257e743f5688c5a09e9b38e
PotatoOwO-001 <chinghungben@gmail.com>
2025-11-16 02:07:46 +0800

    Update character_parameter.py

--- character_parameter.py
+++ character_parameter.py
@@ -4,6 +4,14 @@ import math
 
 pygame.init()
 
+# Screen dimensions
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Moveable Stick Man - A (Left) D (Right)")
+
+# Colors
+BLACK = (0, 0, 0)
+
 #character appearence
 class StickMan:
     def __init__(self, x, y):
@@ -20,16 +28,32 @@ class StickMan:
 
         #movement animation
         self.walking = false
-        self.walking_01 = 0
+        self.walking_patteren = 0
         self.walking_speed = 0.5
+        
         #keyboard_movement_control
         def right_handside_movement(self):
             self.x +=self.speed
-            self.facing_direction = 1
+            self.facing_direction= 1
             self.walking = true
         
         def left_handside_movement(self):
             self.x -=self.speed
             self.facing_direction = -1
             self.walking = true
+            
+        def stop(self):
+            self.walking= false
+            
+        #movement posture refreshment   
+        def update(self):
+            self.walking_patteren += self.walking_speed
+
+        
+            
+        
+            
+
+# Boundary limitation
+size.x = width-size.head_radiu
             
